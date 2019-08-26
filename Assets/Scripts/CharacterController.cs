ce0751a65afa380099d9ab6593ddfce66a3f4f78
Martin <zexyqag@gmail.com>
2019-08-26 13:24:25 +0200

    Added CharaterController and CharaterGrounding script

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(CharacterGrounding))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed = 5;
+	[SerializeField]
+	private float jumpForce = 10;
+
+	public float Speed { get; private set; }
+
+	private new Rigidbody2D rigidbody2D;
+	private CharacterGrounding characterGrounding;
+
+	private void Awake() {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		characterGrounding = GetComponent<CharacterGrounding>();
+	}
+
+	private void Update() {
+		if(Input.GetButtonDown("Fire1") && characterGrounding.IsGrounded) {
+			rigidbody2D.AddForce(Vector2.up * jumpForce * rigidbody2D.mass, ForceMode2D.Impulse);
+		}
+	}
+
+	private void FixedUpdate() {
+		moveCharacter(new Vector2(Input.GetAxisRaw("Horizontal"), 0));
+	}
+
+	private void moveCharacter(Vector2 direction) {
+		rigidbody2D.position += direction * Time.deltaTime * moveSpeed;
+	}
+}
---
+++ Assets/Scripts/CharacterGrounding.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CharacterGrounding : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+	private Transform groundedObject;
+	public bool IsGrounded { get; private set; }
+
+	[SerializeField]
+	private LayerMask groundedLayer;
+	private Vector2 origin, size, sizeOffsetY;
+
+
+	private void Awake() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	private void Update() {
+		sizeOffsetY = (Vector2.up * spriteRenderer.size.y * 0.9f);
+		size = spriteRenderer.size - sizeOffsetY;
+		origin = spriteRenderer.transform.position + (Vector3.down * spriteRenderer.size.y * 0.5f) - size.y * Vector3.up * 0.5f;
+
+		var raycastHit = Physics2D.BoxCast(origin, size, 0f, Vector2.up, Mathf.Infinity, groundedLayer);
+		if(raycastHit.collider != null) {
+			IsGrounded = true;
+		} else {
+			IsGrounded = false;
+		}
+	}
+
+	private void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireCube(origin, size);
+	}
+}
