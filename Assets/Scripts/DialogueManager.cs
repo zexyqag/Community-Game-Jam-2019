c6ff060c3bedec6be8469bc9d3ecc7c9ed3b6439
Marcus <m.h.otterstreom@gmail.com>
2019-08-30 01:53:14 +0200

    Added move event to level "test1"
    Made chat ding
    added temp enemy and spike

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,13 +5,21 @@ public class DialogueManager : MonoBehaviour {
 	[SerializeField] private TextMeshProUGUI Console = null, Chat = null;
 	public static DialogueManager Instance;
 
-	private void Awake() => Instance = this;
+    private AudioSource audioSource;
 
-	public void ConsoleAdd(string text) {
+    private void Awake()
+    {
+        Instance = this;
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+
+    public void ConsoleAdd(string text) {
 		Console.text += "\n" + text;
 	}
 	public void ChatAdd(string text) {
 		Chat.text += "\n\n" + ">" + text;
+        audioSource.Play();
 	}
 
 }
